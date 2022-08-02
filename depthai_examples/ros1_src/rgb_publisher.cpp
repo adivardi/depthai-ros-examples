6a87d6a04efc31b23b4efd0a13318fe436f34049
Adi Vardi <adi.vardi@enway.ai>
2022-08-02 18:40:01 +0800

    test rgb, set fps

--- depthai_examples/ros1_src/rgb_publisher.cpp
+++ depthai_examples/ros1_src/rgb_publisher.cpp
@@ -17,10 +17,14 @@ dai::Pipeline createPipeline(){
     auto colorCam = pipeline.create<dai::node::ColorCamera>();
     auto xlinkOut = pipeline.create<dai::node::XLinkOut>();
     xlinkOut->setStreamName("video");
-    
+
     colorCam->setPreviewSize(300, 300);
     colorCam->setResolution(dai::ColorCameraProperties::SensorResolution::THE_1080_P);
+    // THE_4_K: image_height: 2160 image_width: 4096
+    // THE_1080_P: image_height: 1080 image_width: 1920
     colorCam->setInterleaved(false);
+    colorCam->setFps(60);
+    std::cout << "res: " << colorCam->getResolutionWidth() << " " << colorCam->getResolutionHeight() << std::endl;
 
     // Link plugins CAM -> XLINK
     colorCam->video.link(xlinkOut->input);
@@ -31,7 +35,7 @@ int main(int argc, char** argv){
 
     ros::init(argc, argv, "rgb_node");
     ros::NodeHandle pnh("~");
-    
+
     std::string tfPrefix;
     std::string camera_param_uri;
     int badParams = 0;
@@ -43,22 +47,22 @@ int main(int argc, char** argv){
     {
         throw std::runtime_error("Couldn't find one of the parameters");
     }
-    
+
     dai::Pipeline pipeline = createPipeline();
     dai::Device device(pipeline);
     std::shared_ptr<dai::DataOutputQueue> imgQueue = device.getOutputQueue("video", 30, false);
-    
+
     std::string color_uri = camera_param_uri + "/" + "color.yaml";
 
     dai::rosBridge::ImageConverter rgbConverter(tfPrefix + "_rgb_camera_optical_frame", false);
     dai::rosBridge::BridgePublisher<sensor_msgs::Image, dai::ImgFrame> rgbPublish(imgQueue,
-                                                                                  pnh, 
+                                                                                  pnh,
                                                                                   std::string("color/image"),
-                                                                                  std::bind(&dai::rosBridge::ImageConverter::toRosMsg, 
-                                                                                  &rgbConverter, // since the converter has the same frame name
-                                                                                                  // and image type is also same we can reuse it
-                                                                                  std::placeholders::_1, 
-                                                                                  std::placeholders::_2) , 
+                                                                                  std::bind(&dai::rosBridge::ImageConverter::toRosMsg,
+                                                                                            &rgbConverter,  // since the converter has the same frame name
+                                                                                                            // and image type is also same we can reuse it
+                                                                                            std::placeholders::_1,
+                                                                                            std::placeholders::_2),
                                                                                   30,
                                                                                   color_uri,
                                                                                   "color");
